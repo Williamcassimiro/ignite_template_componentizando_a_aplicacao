b1e1e3b2a2a83e52eb8433dbf50b39abb48eab00
William Cassimiro <willian_cassimiro13@hotmail.com>
2021-05-17 01:06:17 -0300

    ✔ Finished
    ✔ Finished

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,14 @@
-export function Content() {
-  // Complete aqui
+import {Header} from "./Header";
+import {MoviesList} from "./MoviesList";
+
+export function Content(){
+  return (
+    <div className="container">
+      <Header/>
+      <main>
+        <MoviesList/>
+      </main>
+    </div>
+  );
 }
+
